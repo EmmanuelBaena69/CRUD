c2e52f6ff833dadb89f956bf9120fd0cfd7e66d2
SakuSaku <travispastrana075@gmail.com>
2023-05-16 17:49:25 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblVentas = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.dbgrd = new System.Windows.Forms.DataGridView();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnLeer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dbgrd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblVentas
+            // 
+            this.lblVentas.AutoSize = true;
+            this.lblVentas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVentas.Location = new System.Drawing.Point(370, 48);
+            this.lblVentas.Name = "lblVentas";
+            this.lblVentas.Size = new System.Drawing.Size(80, 25);
+            this.lblVentas.TabIndex = 0;
+            this.lblVentas.Text = "Ventas";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecio.Location = new System.Drawing.Point(437, 117);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(73, 25);
+            this.lblPrecio.TabIndex = 1;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(158, 117);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(87, 25);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(268, 114);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(144, 30);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrecio.Location = new System.Drawing.Point(540, 112);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(144, 30);
+            this.txtPrecio.TabIndex = 4;
+            // 
+            // dbgrd
+            // 
+            this.dbgrd.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dbgrd.Location = new System.Drawing.Point(60, 161);
+            this.dbgrd.Name = "dbgrd";
+            this.dbgrd.RowHeadersWidth = 51;
+            this.dbgrd.RowTemplate.Height = 24;
+            this.dbgrd.Size = new System.Drawing.Size(686, 222);
+            this.dbgrd.TabIndex = 5;
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCrear.Location = new System.Drawing.Point(119, 397);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(126, 41);
+            this.btnCrear.TabIndex = 6;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(558, 397);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(126, 41);
+            this.btnEliminar.TabIndex = 7;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnActualizar.Location = new System.Drawing.Point(411, 397);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(126, 41);
+            this.btnActualizar.TabIndex = 8;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLeer.Location = new System.Drawing.Point(268, 397);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(126, 41);
+            this.btnLeer.TabIndex = 9;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.dbgrd);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.lblVentas);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dbgrd)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblVentas;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.DataGridView dbgrd;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnLeer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace CRUD
+{
+    public partial class Form1 : Form
+    {
+        //Algunos comandos necesarios para la conexion
+        SQLiteConnection conexion_sqlite;
+        SQLiteCommand cmd_sqlite;
+        SQLiteDataReader datareader_sqlite;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            //Para verificar campos vacios
+            if (txtName.Text.Trim() != "" && txtPrecio.Text.Trim() != "")
+            {
+                try
+                {
+                    //Conexion a la base de datos
+                    conexion_sqlite = new SQLiteConnection("Data Source=productos.db;Version=3;");
+
+                    //Abrimos la conexion
+                    conexion_sqlite.Open();
+
+                    cmd_sqlite = conexion_sqlite.CreateCommand();
+
+
+                    //Insertando valores, no se inserta el id ya que es auto incrementable
+                    cmd_sqlite.CommandText = string.Format("INSERT INTO tbl_products (nom, precio) VALUES ('" + txtName.Text + "', " + txtPrecio.Text + ")");
+
+                    datareader_sqlite = cmd_sqlite.ExecuteReader();
+
+                    conexion_sqlite.Close();
+                    MessageBox.Show("Registrado Correctamente");
+
+                }
+                catch (Exception iu)
+                {
+
+                    MessageBox.Show("Error al intentar registrarse" + iu.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor llene los campos");
+            }
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            //Codigo para Leer los datos
+
+            conexion_sqlite = new SQLiteConnection("Data Source=productos.db;Version=3;");
+            conexion_sqlite.Open();
+                try
+                {
+                    //Codigo para leer o ver la base de datos en el DataGridView
+                    string sql = "SELECT * FROM tbl_products";
+                    SQLiteCommand command = new SQLiteCommand(sql, conexion_sqlite);
+                    SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    // Cierra la conexión
+                    conexion_sqlite.Close();
+
+                    /*llamamos el datatable que representa una tabla de datos
+                    y nos permite buscar datos y mostralos en el dbgrd (DataGridView)*/
+                    dbgrd.DataSource = dataTable;
+
+                }
+                catch (Exception a)
+                {
+
+                    MessageBox.Show("Error" + a.Message);
+                }
+            }
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            //Codigo para actualizar los datos
+
+            conexion_sqlite = new SQLiteConnection("Data Source=productos.db;Version=3;");
+            conexion_sqlite.Open();
+
+            if (txtName.Text.Trim() != "" && txtPrecio.Text.Trim() != "")
+            {
+                try
+                {
+                    //lanzamos el comando update table que normalmente se usa en las DBS
+                    SQLiteCommand cmd = new SQLiteCommand("UPDATE tbl_products SET nom=@Nombre WHERE precio=@Precio", conexion_sqlite);
+                    cmd.Parameters.AddWithValue("@Nombre", txtName.Text);
+                    cmd.Parameters.AddWithValue("@Precio", txtPrecio.Text);
+
+                    cmd.ExecuteNonQuery();
+                    conexion_sqlite.Close();
+
+                    MessageBox.Show("Actualizado correctamente por favor lea los datos de nuevo");
+                }
+                catch (Exception l)
+                {
+
+                    MessageBox.Show("Error" + l.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor llene los campos");
+            }
+        }
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            //Codigo para eliminar datos de la tabla
+
+            conexion_sqlite = new SQLiteConnection("Data Source=productos.db;Version=3;");
+            conexion_sqlite.Open();
+
+            if (txtName.Text.Trim() != "")
+            {
+                try
+                {
+                    //lanzamos el comando delete from table que normalmente se usa en las DBS
+                    SQLiteCommand cmd = new SQLiteCommand("DELETE FROM tbl_products WHERE nom=@Nombre", conexion_sqlite);
+                    cmd.Parameters.AddWithValue("@Nombre", txtName.Text);
+                    cmd.ExecuteNonQuery();
+                    conexion_sqlite.Close();
+
+                    MessageBox.Show("Se ha eliminado correctamente por favor lea los datos de nuevo");
+                }
+                catch (Exception l)
+                {
+
+                    MessageBox.Show("Error al borrar los datos " + l.Message);
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor llene los campos");
+            }
+        }
+    }
+}
